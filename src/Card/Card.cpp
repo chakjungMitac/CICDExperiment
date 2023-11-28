8fad0187eced6b498b6d14a6d749b332948b98e9
Jung Chak <chakjung@mitac.com.tw>
2023-11-28 21:35:18 +0800

    Add expireTime for Card

--- src/Card/Card.cpp
+++ src/Card/Card.cpp
@@ -1,5 +1,9 @@
 #include "Card.h"
 
+#include <iomanip>  // std::put_time
+
+#include <glog/logging.h>  // LOG(*)
+
 Card::Card(std::chrono::time_point<std::chrono::high_resolution_clock> expireTime)
     : expireTime(expireTime) {}
 
@@ -8,4 +12,14 @@ Card::getExpireTime() {
     return expireTime;
 }
 
-void Card::logInfo() {}
+void Card::logInfo() {
+    LOG(INFO) << "=================== Card Info ===================";
+
+    // Log ExpireTime
+    time_t expireTime_time_t =
+        std::chrono::high_resolution_clock::to_time_t(expireTime);
+    LOG(INFO) << "ExpireTime: "
+              << std::put_time(std::gmtime(&expireTime_time_t), "%F %T");
+
+    LOG(INFO) << "=================================================";
+}
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,17 @@ int main() {
         google::InitGoogleLogging("CICDExperiment");
     }
 
+    // Construct card expireTime (2023/08/15 13:32:08)
+    std::chrono::year_month_day expireDate(
+        std::chrono::year(2023), std::chrono::August, std::chrono::day(15));
+    std::chrono::hh_mm_ss expireTimeOfDate(
+        std::chrono::hours(13) + std::chrono::minutes(32) + std::chrono::seconds(8));
+    std::chrono::time_point<std::chrono::high_resolution_clock> expireTime(
+        std::chrono::sys_days{expireDate} + expireTimeOfDate.to_duration());
+
+    Card card(expireTime);
+    card.logInfo();
+
     google::ShutdownGoogleLogging();
     return EXIT_SUCCESS;
 }
