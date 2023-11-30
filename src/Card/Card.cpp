f72d15b84e35607d34d20d315efb96e9b6c4fa5a
Jung Chak <chakjung@mitac.com.tw>
2023-11-30 10:31:23 +0800

    Add fields for Card
    Add cardStatus
    Add transitStationNums

--- src/Card/Card.cpp
+++ src/Card/Card.cpp
@@ -13,6 +13,25 @@ const int& Card::getBalance() { return balance; }
 
 void Card::setBalance(const int& balance) { this->balance = balance; }
 
+const Card::CardStatus& Card::getCardStatus() { return cardStatus; }
+
+void Card::setCardStatus(const CardStatus& cardStatus) {
+    if (cardStatus != InTransit) {
+        transitStationNums.InboundStation = 0;
+        transitStationNums.OutboundStation = 0;
+    }
+
+    this->cardStatus = cardStatus;
+}
+
+const Card::TransitStationNums& Card::getTransitStationNums() {
+    return transitStationNums;
+}
+
+void Card::setTransitStationNums(const TransitStationNums& transitStationNums) {
+    this->transitStationNums = transitStationNums;
+}
+
 void Card::logInfo() {
     LOG(INFO) << "=================== Card Info ===================";
 
@@ -22,8 +41,16 @@ void Card::logInfo() {
     LOG(INFO) << "ExpireTime: "
               << std::put_time(std::gmtime(&expireTime_time_t), "%F %T");
 
-    // Log balance
-    LOG(INFO) << "Balance: " << balance;
+    // Log Balance
+    LOG(INFO) << "Balance   : " << balance;
+
+    // Log CardStatus
+    LOG(INFO) << "CardStatus: " << cardStatus;
+
+    // Log TransitStationNums
+    LOG(INFO) << "TransitStationNums";
+    LOG(INFO) << "   Inbound: " << transitStationNums.InboundStation;
+    LOG(INFO) << "  Outbound: " << transitStationNums.OutboundStation;
 
     LOG(INFO) << "=================================================";
 }
--- src/Card/Card.h
+++ src/Card/Card.h
@@ -3,6 +3,14 @@
 #include <chrono>
 
 class Card {
+   public:
+    enum CardStatus { Normal, InTransit };
+
+    struct TransitStationNums {
+        unsigned int InboundStation;
+        unsigned int OutboundStation;
+    };
+
    private:
     typedef std::chrono::time_point<std::chrono::high_resolution_clock> ExpireTime;
 
@@ -14,10 +22,24 @@ class Card {
     const int& getBalance();
     void setBalance(const int& balance);
 
+    const CardStatus& getCardStatus();
+    void setCardStatus(const CardStatus& cardStatus);
+
+    const TransitStationNums& getTransitStationNums();
+    void setTransitStationNums(const TransitStationNums& transitStationNums);
+
     void logInfo();
 
    private:
     ExpireTime expireTime;
 
     int balance;
+
+    CardStatus cardStatus = CardStatus::Normal;
+
+    /**
+     * InTransit: (Inbound station, Outbound station)
+     * Else: (0, 0)
+     */
+    TransitStationNums transitStationNums{0, 0};
 };
